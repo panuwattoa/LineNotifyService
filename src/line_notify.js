5f8a78b30e9825633cfd5e23c550e7b4adf5fceb
ess <panuwat@digitopolisstudio.com>
2020-08-02 18:36:56 +0700

    feat: add line api replyMessage and Notify

---
+++ src/line_notify.js
@@ -0,0 +1,47 @@
+
+
+import { ensureJSON, toArray } from "./utils";
+
+class Notify{
+    constructor({token}) {
+		if(!token) {
+			throw new Error('invalid with {token} argument.')
+        }
+        this.http = axios.create();
+        this.http.defaults.headers.common['Authorization'] = "Bearer " + token;
+    }
+    
+    
+    async Notify(args){
+        return new Promise((resolve, reject)=>{
+            this.http.post(config.URL_LINE['send'], {
+                messages: toArray(args.messages),
+                replyToken: args.replyToken,
+              }).then(res=>{
+                const body = res.body
+                resolve(JSON.parse(body))
+            })
+            .catch(err=>{
+                reject(JSON.parse(err.error))
+            });
+        })
+    }
+
+    replyMessage(args){
+        return new Promise((resolve, reject)=>{
+            this.http.post(`${config.MESSAGING_API_PREFIX}/message/reply`, {
+                messages: toArray(args.messages),
+                replyToken: args.replyToken,
+              }).then(res=>{
+                const body = res.body
+                resolve(JSON.parse(body))
+            })
+            .catch(err=>{
+                reject(JSON.parse(err.error))
+            });
+        })
+    }
+
+}
+
+export default Notify

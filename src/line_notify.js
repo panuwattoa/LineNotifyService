5fb6b1ffcda4362d36b8903d7ddbcbe958177b4e
ess <panuwat@digitopolisstudio.com>
2020-08-03 15:23:43 +0700

    fix: update code

--- src/line_notify.js
+++ src/line_notify.js
@@ -1,46 +1,44 @@
-
-
-import { ensureJSON, toArray } from "./utils";
-
+import axios from 'axios'
+import config from './config.js'
 class Notify{
-    constructor({token}) {
-		if(!token) {
-			throw new Error('invalid with {token} argument.')
-        }
-        this.http = axios.create();
-        this.http.defaults.headers.common['Authorization'] = "Bearer " + token;
-    }
     
+    constructor({tokenNotify, tokenBot }) {
+        this.http = axios.create();
+        this.tokenNotify = tokenNotify
+        this.tokenBot = tokenBot
+    }
+
     //https://developers.line.me/businessconnect/api-reference#sending_message
     Notify(args){
         return new Promise((resolve, reject)=>{
-            this.http.post(config.URL_LINE['send'], {
+            this.http.defaults.headers.common['Authorization'] = "Bearer " + this.tokenNotify;
+            this.http.post(config.URL_LINE.SEND, {
                 messages: args.messages,
               }).then(res=>{
                 const body = res.body
-                resolve(JSON.parse(body))
+                resolve(body)
             })
             .catch(err=>{
-                reject(JSON.parse(err.error))
+                reject(err.error)
             });
         })
     }
 
     replyMessage(args){
         return new Promise((resolve, reject)=>{
-            this.http.post(`${config.MESSAGING_API_PREFIX}/message/reply`, {
-                messages: toArray(args.messages),
+            this.http.defaults.headers.common['Authorization'] = "Bearer " + this.tokenBot;
+            this.http.post(`${config.URL_LINE.MESSAGING_API_PREFIX}/message/reply`, {
+                messages: args.messages,
                 replyToken: args.replyToken,
               }).then(res=>{
                 const body = res.body
-                resolve(JSON.parse(body))
+                resolve(body)
             })
             .catch(err=>{
-                reject(JSON.parse(err.error))
+                reject(err.error)
             });
         })
     }
-
 }
 
 export default Notify
